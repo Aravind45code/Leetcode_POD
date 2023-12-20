b67889e1171e2dcd2c28fb0226e8ec4acdcd0fb5
Aravind45code <aravindreddyveerelli@gmail.com>
2023-12-19 21:48:37 -0600

    change

--- 19thDEC.cpp
+++ 19thDEC.cpp
@@ -10,3 +10,13 @@ public:
         return money;
     }
 };
+// same code in java
+// class Solution {
+//     public int buyChoco(int[] prices, int money) {
+//         Arrays.sort(prices);
+//         if(money-(prices[0]+prices[1])>=0){
+//             return money-(prices[0]+prices[1]);
+//         }
+//         return money;
+//     }
+// }
